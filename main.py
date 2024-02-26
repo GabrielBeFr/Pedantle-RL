4707a3f2ea2ff29d7fd88abe533006e452620cd3
GabrielBeFr <gabriel.benzenou@student-cs.fr>
2024-02-26 19:43:02 +0100

    main file

---
+++ main.py
@@ -0,0 +1,26 @@
+import gym_examples
+import gym
+import time
+
+if __name__ == "__main__":
+    env = gym.make(
+        "gym_examples/Pedantle-v0", 
+        render_mode="human", 
+        test_model=True, 
+        wiki_file="/home/gabriel/cours/RL/projet/wikipedia_april.csv",
+        )
+
+    env.action_space.seed(42)
+
+    observation = env.reset(seed=42)
+
+    actions = ["and","fourth","be","the","of","year","is","be","for","day","always","often","between","come","can","do","in","common","start","first","second","April"]
+    for i in range(20):
+        action = actions[i]
+        time.sleep(2)
+        observation, reward, terminated, _, _ = env.step(action)
+
+        if terminated:
+            observation, info = env.reset()
+
+    env.close()
