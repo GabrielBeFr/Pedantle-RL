034ae188736cba5a75a9bce067058ceddafa3938
gabriel.benzenou@student-cs.fr <gabriel.benzenou@student-cs.fr>
2024-03-21 23:43:57 +0100

    reorganizing agent structure

--- agent/states.py
+++ agent/states.py
@@ -1,10 +1,12 @@
 import numpy as np
 from math import ceil
 
+N_STATES = 100
+
 def compute_state(observation, offset):
     total_words = len(observation["fitted_words"])
     found_words = len(np.where(observation["words_prox"]==1)[0]) # [0] because np.where returns a tuple
-    state = ceil(100 * (found_words-offset) / (total_words-offset)) # we create 100 possible states
-    if state == 100:
-        state = int(100 * (found_words-offset) / (total_words-offset))
+    state = ceil(N_STATES * (found_words-offset) / (total_words-offset)) # we create 100 possible states
+    if state == N_STATES:
+        state = int(N_STATES * (found_words-offset) / (total_words-offset))
     return state
